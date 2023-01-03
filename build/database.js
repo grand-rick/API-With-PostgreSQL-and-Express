fa1003970fca97c9093849bad11e940ca5ef6930
Patrick Kabuga <kabugap02@gmail.com>
2023-01-03 05:23:07 +0300

    Transpiling

---
+++ build/database.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
+var pg_1 = require("pg");
+dotenv_1.default.config();
+var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV, POSTGRES_TEST_DB = _a.POSTGRES_TEST_DB;
+// @ts-ignore
+var client = new pg_1.Pool();
+console.log(ENV);
+if (ENV === 'test') {
+    client = new pg_1.Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    });
+}
+if (ENV === 'dev') {
+    client = new pg_1.Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_USER,
+        password: POSTGRES_PASSWORD
+    });
+}
+exports.default = client;
