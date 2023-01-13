d5d932364d810efd5f6532742b7c399ce0c2012c
Patrick Kabuga <kabugap02@gmail.com>
2023-01-13 06:25:50 +0300

    Imported the users routes into the server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import book_store_routes from './handlers/books';
+import usersRoutes from './handlers/users';
+import cors from 'cors';
+import morgan from 'morgan';
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const port: number = 3000;
+const corsOptions = {
+    origin: 'https://someotherdomain.com',
+    optionsSuccessStatus: 200
+}
 
-app.use(bodyParser.json())
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(morgan('common'));
+
+book_store_routes(app);
+usersRoutes(app);
 
 app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+app.listen(port, function () {
+    console.log(`starting app on: http://localhost${port}`);
+});
